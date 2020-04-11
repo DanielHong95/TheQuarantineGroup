c570fd959f9baca3a7a304a8e0bc2a6af4e637ed
nickszabo21 <57157636+nickszabo21@users.noreply.github.com>
2020-04-11 07:11:43 -0500

    Delete unemployment.py

--- unemployment.py
+++
@@ -1,9 +0,0 @@
-
-from matplotlib import pyplot as plt
-from matplotlib import style
-from numpy import genfromtxt
-
-data = genfromtxt('Data/unemployment.csv',delimiter=' ')
-
-plt.plot(data)
-plt.show
